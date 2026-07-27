b1ced6e10ded058437274bc22c00f0d88d34215f
Vanshika Varshney <varshney23queen11@gmail.com>
2026-07-27 14:34:09 +0530

    Add files via upload

---
+++ receivingInputFromUser.c
@@ -0,0 +1,13 @@
+// LIBRARY FUNCTION AND RECEIVING INPUT FROM USER
+
+#include <stdio.h>
+
+int main() {
+    int i;
+    printf( "Enter the value of i: ");
+    
+    scanf("%d" , &i);
+    printf("The value of i is: %d\n ", i );
+    
+    return 0;
+}
