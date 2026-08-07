c5f04ce797608e330a6b8f30f72184e2eb3a57d7
Vanshika Varshney <varshney23queen11@gmail.com>
2026-08-07 07:17:26 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,11 @@
+// Array 
+#include <stdio.h>
+
+int main() {
+    int marks[89] = {34, 56, 78, 46, 78, 36, 82, 90, 29, 67, 33, 55, 99,100};
+    printf("Marks 0, Marks 4 and Marks 8 are %d %d %d", marks[0], marks[4], marks[8]);
+    int sum;
+    
+    
+    return 0;
+}
