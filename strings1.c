b6247e511836fc884162e3de3739ef6c984691fc
Vanshika Varshney <varshney23queen11@gmail.com>
2026-08-09 12:36:44 +0530

    Add files via upload

---
+++ strings1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  
+ char ur_frnd[50];
+ 
+ 
+ fgets(ur_frnd, sizeof(ur_frnd), stdin);
+ 
+// printf"Tell ur best friend's name:   \n");
+  
+ printf("Your best friend is of type beautyWithBrain and his or her name is %s\n", ur_frnd);
+ 
+ return 0;
+ 
+ 
+}
